f3e39120e44b18690e6fc3768fa05f220300d3a6
praveenr2998 <51623786+praveenr2998@users.noreply.github.com>
2021-03-19 22:28:22 +0530

    Update fashion_mnist.py

--- 03-Image Dataset Linear Model/fashion_mnist.py
+++ 03-Image Dataset Linear Model/fashion_mnist.py
@@ -23,7 +23,7 @@ tr_targets = fmnist.targets
 
 class CustomDataset(Dataset):
     def __init__(self, x, y):
-        x = x.float()
+        x = x.float()/255        # Normalisation
         x = x.view(-1, 28*28)
         self.x, self.y = x, y
 
